5a5652080967b4a38dc71214a846943a1f2a0602
Billrawknow <bildadronoh@gmail.com>
2023-10-26 08:49:25 +0300

    register.php

--- register.php
+++ register.php
@@ -11,7 +11,6 @@
 		<link rel="shortcut icon" href="images/favicon.png" />
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 	</head>
-
 	<body>
 		<div class="container-scroller">
 			<div class="container-fluid page-body-wrapper full-page-wrapper auth-page">
